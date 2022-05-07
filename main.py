45998ad4e05c787af8b40af32894e6f1327e06a1
ViktorShamonin <vshamonin05@gmail.com>
2022-05-07 23:45:59 +0300

    'n32'

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ class Dialog():
                 else:
                     self.response['response']['text'] = 'Вы готовы начать ' + ('прогулку'  if not self.car else 'поездку') + '?'
                     self.stage = 5
-                    self.make_info(self.street, self.car)
+                    self.make_info(self.city + ', ' + self.street, self.car)
             except:
                 self.response['response']['text'] = 'Я не смогла найти вашу улицу. Пожалуйста, повторите.'
                 return
